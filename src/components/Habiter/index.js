76dacc34d258712f44c40a6f46edfeffdb971e75
Corxys <jennblngr@gmail.com>
2021-05-19 17:00:07 +0200

    creation of a Habiter component, installation of Sass

---
+++ src/components/Habiter/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Habiter = () => {
+  return (
+    <div className="container">
+      TODO: ici le site
+    </div>
+  )
+};
+
+export default Habiter;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+
+import './styles.scss';
+
+import Habiter from './components/Habiter';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootReactElement = (
+  <Habiter />
 );
+
+const target = document.querySelector('#root');
+render(rootReactElement, target);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
